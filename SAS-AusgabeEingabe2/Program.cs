d0e146d0b59efdae4cddc8b8528cb2687b8661d6
Nicolai Kohl <nicolai.k3@gso.schule.koeln>
2022-09-08 08:46:05 +0200

    SAS Ausgame Eingabe Aufgabe 1 bis 3

--- SAS-AusgabeEingabe2/Program.cs
+++ SAS-AusgabeEingabe2/Program.cs
@@ -1,1 +1,31 @@
-﻿string Vorname = "Nicolai"
+﻿//Aufgabe 1
+
+Console.WriteLine(" _-_ ");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+//Aufgabe 2
+
+string Vorname = "Nicolai";
+string Nachname = "Kohl";
+string Alter = "16";
+
+Console.WriteLine("Vorname: {0}", Vorname);
+Console.WriteLine("Nachname: {0}", Nachname);
+Console.WriteLine("Alter: {0}", Alter);
+
+//Aufgabe 3
+
+string vorname;
+string alter;
+string klasse;
+
+Console.WriteLine("Geben sie ihre Daten ein!");
+
+vorname = Console.ReadLine();
+alter = Console.ReadLine();
+klasse = Console.ReadLine();
+
+Console.WriteLine($"{vorname} ist {alter} Jahre alt und besucht die {klasse} des Georg-Simon-Ohm Berufskolleg");
